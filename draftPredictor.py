1acfd44aae4b814efc5a4bdc62fbcc4b55fdf08e
noamgreenstein <106999318+noamgreenstein@users.noreply.github.com>
2023-01-08 02:01:48 -0500

    all the math is done. just need to input data!

--- draftPredictor.py
+++ draftPredictor.py
@@ -1,10 +1,3 @@
-# use 2007-2011 drafts to develop a solid prediction model for NBA Draft
-# College stats taken into account: PPG, APG, RPG, SPG, BPG, FG%, Pos, MPG
-# Success measure: Stats, Games Played, Years in NBA, Money Earned - combine to some metric?
-# Step 1: Use Drafts 07-11 as training, 2012,13 as test
-# - Make an object with all relevant info? How to Plot? Equation?
-# Step 2: Plot 7-11, 12,13
-# Step 3: Calculate MSE to show similarity and efficiency of model
 import math
 from enum import Enum
 
