224017af107b592dbb9488f727e4b2ffcbed4738
noamgreenstein <106999318+noamgreenstein@users.noreply.github.com>
2023-01-14 15:18:20 -0500

    testing and generating results

--- webScraper.py
+++ webScraper.py
@@ -18,35 +18,29 @@ def get_data(url, pos, all_star, all_nba):
 
 
 
+
 def get_totals(div):
     footer = div.find('tfoot')
     tr = footer.find('tr')
     pts = tr.prettify().split('<td class="right" data-stat="pts">')
     points = pts[1][3:len(pts[1]) - 13]
-    print(float(points))
     try:
         trash = pts[0].split('<td class="right" data-stat="tov">')
         blk = trash[0].split('<td class="right" data-stat="blk">')
         blocks = blk[1][:len(blk[1]) - 7]
-        print(float(blocks))
     except ValueError:
         trash = pts[0].split('<td class="right iz" data-stat="tov">')
         blk = trash[0].split('<td class="right" data-stat="blk">')
         blocks = blk[1][:len(blk[1]) - 7]
-        print(float(blocks))
     except IndexError:
         blk = trash[0].split('<td class="right iz" data-stat="blk">')
         blocks = blk[1][:len(blk[1]) - 7]
-        print(float(blocks))
     stl = blk[0].split('<td class="right" data-stat="stl">')
     steals = stl[1][:len(stl[1]) - 7]
-    print(float(steals))
     ast = stl[0].split('<td class="right" data-stat="ast">')
     assists = ast[1][:len(ast[1]) - 7]
-    print(float(assists))
     trb = ast[0].split('<td class="right" data-stat="trb">')
     rebounds = trb[1][:len(trb[1]) - 7]
-    print(float(rebounds))
     return [float(points), float(rebounds), float(assists), float(steals), float(blocks)]
 
 
@@ -55,27 +49,20 @@ def get_college(div):
     end_out = tfoot[1].split("</tr>")
     ast = end_out[0].split('<td class="right " data-stat="ast_per_g" >')
     assists = ast[1][:len(ast[1])-5]
-    print(float(assists))
     trb = ast[0].split('<td class="right " data-stat="trb_per_g" >')
     rebounds = trb[1][:len(trb[1])-5]
-    print(float(rebounds))
     pts = trb[0].split('<td class="right " data-stat="pts_per_g" >')
     points = pts[1][:len(pts[1])-5]
-    print(float(points))
     fg = pts[0].split('<td class="right " data-stat="fg_pct" >')
     fg_pct = fg[1][:len(pts[1])-5]
-    print(float(fg_pct))
     trash = fg[0].split('<td class="right " data-stat="tov" >')
     blk = trash[0].split('<td class="right " data-stat="blk" >')
     blocks = blk[1][:len(blk[1])-5]
-    print(float(blocks))
     stl = blk[0].split('<td class="right " data-stat="stl" >')
     steals = stl[1][:len(stl[1])-5]
-    print(float(steals))
     more_trash = stl[0].split('<td class="right " data-stat="mp" >')
     g = more_trash[0].split('<td class="right " data-stat="g" >')
     games = g[1][:len(ast[1])-6]
-    print(float(games))
 
     return [float(points), float(rebounds), float(assists), float(fg_pct),
             float(steals)/float(games), float(blocks)/float(games)]
