839f204e319bee5eb14430a115dfac5797e48c1d
noamgreenstein <106999318+noamgreenstein@users.noreply.github.com>
2023-01-14 15:09:44 -0500

    testing and generating results

--- playerDatabase.py
+++ playerDatabase.py
@@ -50,5 +50,4 @@ training_2007 = numpy.array(
      webScraper.get_data('https://www.basketball-reference.com/players/m/mcguido01.html', 2, 0, 0),
      webScraper.get_data('https://www.basketball-reference.com/players/g/grayaa01.html', 2, 0, 0),
      webScraper.get_data('https://www.basketball-reference.com/players/g/greenta01.html', 1, 0, 0),
-     webScraper.get_data('https://www.basketball-reference.com/players/n/nichode01.html', 2, 0, 0),
      draftPredictor.Player(1, 8.8, 4.2, 4.9, .417, 1.2, 0.14, 7005, 1802, 2730, 458, 50, 0, 0)])
